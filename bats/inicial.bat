659b85fbc571aac675d20df37278d2b2301b72a5
Darasukan <giovannysouza233@gmail.com>
2023-05-03 16:29:07 -0300

    Quarto Commit

---
+++ bats/inicial.bat
@@ -0,0 +1,12 @@
+@echo off
+cd \
+cd "AppsInicial"
+echo Iniciando Steam
+start /b /min Steam.lnk
+echo Iniciando Spotify
+start /b /min Spotify.lnk
+echo Iniciando Opera
+start /b /min Opera.lnk
+echo Iniciando Chrome
+start /b /min Chrome.lnk
+echo Finalizado
